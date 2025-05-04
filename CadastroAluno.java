36828c9a202c5e4ea818eb1e427656a61377f0f4
Tecnologia Sustentaveis <matheusmp8604@gmail.com>
2025-05-03 21:05:04 -0300

    Fiz até o 2 que é remover disciplinas

--- CadastroAluno.java
+++ CadastroAluno.java
@@ -2,64 +2,184 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class CadastroAluno {
+    
+    public static void init(Aluno[] alunos , Disciplina[] disciplinas){
+        Random rnd = new Random();
+        String[] nomesDisciplinas = {"MAT", "HIS", "POO"};
+        String[] professores = {"Matheus", "Carla", "Gustavo"};
+        String[] aluno = {"Carlos", "Lucas", "Pedro", "Luciano", "Mateus"};
+        String[] enderecos = {"Rua 1", "Rua 2", "Rua 3"};
+        String[] anos = {"2025", "2024", "2023"};
+
+        int cont = 0;
+
+        for (int i = 0; i < 3; i++){
+            disciplinas[i].cod = i + 1;
+            disciplinas[i].nome = nomesDisciplinas[rnd.nextInt(nomesDisciplinas.length)];
+            disciplinas[i].ano = anos[rnd.nextInt(anos.length)];
+            disciplinas[i].NomeProf = professores[rnd.nextInt(professores.length)];
+            
+        }
+
+
+        for (int i = 0; i < 5; i++) {
+            alunos[i].cod = i + 1;
+            alunos[i].nome = aluno[rnd.nextInt(aluno.length)];
+            if(i != 0){
+            while(cont == 2){
+                for(int j = 0; j < i; j++){
+                    if (alunos[i].nome == alunos[j].nome) {
+                        cont++;
+                    }   
+                }
+                if(cont == 2){
+                   alunos[i].nome = aluno[rnd.nextInt(aluno.length)];
+                   cont = 0; 
+                }  
+            }
+        }
+            alunos[i].endereco = enderecos[rnd.nextInt(enderecos.length)];
+            alunos[i].DisMatriculado = nomesDisciplinas[rnd.nextInt(nomesDisciplinas.length)];
+            alunos[i].NotasDis = i + 1;
+    }
+
+        menu(disciplinas);
+
+    }
+
+   public static void  CadastrarDisciplina(Disciplina[] disciplina){
+    int j = 0;
+    int aux = 0;
+    for (int i = 0; i < disciplina.length; i++) {
+        if (disciplina[i].nome == null) {
+            j = i;
+            break;
+        }
+    }
+
+    if (j == 0) {
+        System.out.println("Limite de disciplinas atingido.");
+        return;
+    }
+
+    if(aux == 0){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Cadastro de Nova Disciplina");
+        System.out.print("Informe o Codigo ");
+        disciplina[j].cod = scn.nextInt();
+        scn.nextLine(); // limpar buffer
+
+        System.out.print("Nome: ");
+        disciplina[j].nome = scn.nextLine();
+
+        System.out.print("Ano: ");
+        disciplina[j].ano = scn.nextLine();
+
+        System.out.print("Nome do Professor: ");
+        disciplina[j].NomeProf = scn.nextLine();
+
+        System.out.println("Disciplina cadastrada com sucesso!");
+        System.out.println("Se quiser continuar digite 0 senão 1 para sair");
+        aux = scn.nextInt();
+        while((aux < 0) ||(aux > 1) ){
+            System.out.println("Número invalido");
+            aux = scn.nextInt();
+        }
+        
+    }
+
+    }
+
+    public static void RemoverDisciplina(Disciplina[] disciplina) {
+    int aux = 0;
+    if(aux == 0){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Informe o código da disciplina a ser removida: ");
+        int codigo = scn.nextInt();
+    
+        int encontrado = 0;
+    
+        for (int i = 0; i < disciplina.length; i++) {
+            if (disciplina[i].nome != null && disciplina[i].cod == codigo) {
+                // "Remove" limpando os dados
+                disciplina[i] = new Disciplina(); // substitui por um novo objeto vazio
+                encontrado = 1;
+                System.out.println("Disciplina removida com sucesso!");
+                break;
+            }
+        }
+        if (encontrado == 0) {
+            System.out.println("Disciplina não encontrada");
+            System.out.println("Se quiser continuar digite 0 senao 1");
+             aux = scn.nextInt();
+            while((aux < 0) ||(aux > 1) ){
+                System.out.println("Número invalido");
+                aux = scn.nextInt();
+            }
+
+        }
+    }    
+}
+
+
+    public static void menu(Disciplina[] disciplinas) {
+
+        System.out.println("-------------- Menu -------------");
+            System.out.println("1: Cadastrar Disciplina");
+            System.out.println("2: Remover Disciplina");
+            System.out.println("3: Listar Disciplinas (em breve)");
+            System.out.println("4: Cadastrar Aluno (em breve)");
+            System.out.println("5: Alterar Aluno (em breve)");
+            System.out.println("0: Sair");
+            System.out.print("Escolha uma opção: ");
+        Scanner scn = new Scanner(System.in);
+        int opcao = scn.nextInt();
+        while ((opcao < 1) || (opcao > 5)) {
+            System.out.print("Opcao invalida, digite novamente: ");
+            opcao = scn.nextInt();
+        }
+
+        if (opcao == 1) {
+            CadastrarDisciplina(disciplinas);
+        }
+        if (opcao == 2) {
+            RemoverDisciplina(disciplinas);
+        }
+
+    }
+
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
-        System.out.print("Insira o nuemro de linhas da matriz: ");
-        int x = scn.nextInt();
-        while ((x <= 0) || (x > 10)) {
-            System.out.print("O numero deve ser maior que 0 e menor que 10: ");
-            x = scn.nextInt();
+        // inicialização do vetor Aluno com o registro
+        Aluno[] alunos = new Aluno[30];
+        for (int i = 0; i < alunos.length; i++) {
+            alunos[i] = new Aluno();
         }
 
-        System.out.print("Insira o nuemro de colunas da matriz: ");
-        int y = scn.nextInt();
-        while ((y <= 0) || (x > 10)) {
-            System.out.print("O numero deve ser maior que 0 e menor que 10: ");
-            y = scn.nextInt();
+        // inicialização do vetor de empresas com o registro
+        Disciplina[] disciplinas = new Disciplina[10];
+        for (int i = 0; i < disciplinas.length; i++) {
+            disciplinas[i] = new Disciplina();
         }
 
-        int[][] A = new int[x][y];
+        init( alunos, disciplinas);
         
-        //preenche aleatoriamente a matriz
-        Random rnd = new Random();
-        for (x = 0; x < A.length; x++){
-            for (y = 0; y < A[x].length; y++){
-                A[x][y] = rnd.nextInt(10) + 1;
-            }
-        }
-        //imrpime a matriz
-        for (x = 0; x < A.length; x++){
-            System.out.println("");
-            for (y = 0; y < A[x].length; y++){
-               System.out.print(A[x][y] + " ");
-            }
-        }
+}
 
-        int[][] B = novaMatriz(A);
-        System.out.println("");
-        System.out.println("");
+//Registro
+static class Aluno {
+    int cod;
+    String nome;
+    String endereco;
+    String DisMatriculado;
+    double NotasDis;
+}
 
-        //imrpime a matriz
-        for (x = 0; x < B.length; x++){
-            System.out.println("");
-            for (y = 0; y < B[x].length; y++){
-               System.out.print(B[x][y] + " ");
-            }
-        }
-    }
-
-    static int[][] novaMatriz(int[][] a){
-        int[][] b = new int[a.length][a[0].length + 1];
-        for (int x = 0; x < b.length; x++){
-            int temp = 1;
-            for (int y = 0; y < a[x].length; y++){
-               b[x][y] = a[x][y]; 
-               temp *= b[x][y];
-            }
-            b[x][b[x].length - 1] = temp;
-        }
-
-        return b;
-    }
+static class Disciplina {
+    int cod;
+    String nome;
+    String ano;
+    String NomeProf;
+}
 }
